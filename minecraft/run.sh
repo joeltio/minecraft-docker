663fb1021ea6c733361a0b1e46d7a46fca2bd0b7
Joel Tio <joeltiojinhon@gmail.com>
2021-05-23 00:13:04 +0800

    feat: add commands.txt allowing users to send commands

--- minecraft/run.sh
+++ minecraft/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 chown -R minecraft:minecraft /minecraft
 su minecraft
-java -Xmx1024M -Xms1024M -jar server.jar nogui
+# Create or truncate commands.txt
+> commands.txt
+tail -f commands.txt | java -Xmx1024M -Xms1024M -jar server.jar nogui
